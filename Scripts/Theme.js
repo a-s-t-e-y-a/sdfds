8d9a0f7a30fe737defc66cf533339e0a7ac337a7
krishnoit <krishnoit100@gmail.com>
2023-10-06 01:15:24 +0530

    #7 Feat : search functionailty added

--- Scripts/Theme.js
+++ Scripts/Theme.js
@@ -26,3 +26,80 @@ if (storedTheme === 'enabled') {
 
 // Add click event listener to the theme toggle button
 document.querySelector('#btn').addEventListener('click', toggleDarkMode);
+let superheroes = [
+    'Ant-Man',
+    'Aquaman',
+    'Asterix',
+    'The Atom',
+    'The Avengers',
+    'Batgirl',
+    'Batman',
+    'Batwoman',
+    'Black Canary',
+    'Black Panther',
+    'Captain America',
+    'Captain Marvel',
+    'Catwoman',
+    'Conan the Barbarian',
+    'Daredevil',
+    'The Defenders',
+    'Doc Savage',
+    'Doctor Strange',
+    'Elektra',
+    'Fantastic Four',
+    'Ghost Rider',
+    'Green Arrow',
+    'Green Lantern',
+    'Guardians of the Galaxy',
+    'Hawkeye',
+    'Hellboy',
+    'Incredible Hulk',
+    'Iron Fist',
+    'Iron Man',
+    'Marvelman',
+    'Robin',
+    'The Rocketeer',
+    'The Shadow',
+    'Spider-Man',
+    'Sub-Mariner',
+    'Supergirl',
+    'Superman',
+    'Teenage Mutant Ninja Turtles',
+    'Thor',
+    'The Wasp',
+    'Watchmen',
+    'Wolverine',
+    'Wonder Woman',
+    'X-Men',
+    'Zatanna',
+    'Zatara'
+  ];
+  
+  ul = document.getElementById("users-list");
+  
+  let render_lists = function(lists){
+    let li = "";
+    for(index in lists){
+      li += "<li>" + lists[index] + "</li>";
+    }
+    ul.innerHTML = li;
+  }
+  
+  input = document.getElementById('filter_users');
+  let filterUsers = function(event){
+    keyword = input.value.toLowerCase();
+    if(keyword.length!=0){
+      ul.style.display = "block";
+      filtered_users = superheroes.filter(function(user){
+          user = user.toLowerCase();
+         return user.indexOf(keyword) > -1; 
+    });
+    render_lists(filtered_users);
+    }else{
+      ul.style.display = "none";
+    }
+   
+  }
+  
+  input.addEventListener('keyup', filterUsers);
+  
